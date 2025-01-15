763c9635a544810e617c4ef6955d6e93e6cc5d5c
antorio <antorio.bergasdito@gmail.com>
2025-01-16 01:11:57 +0700

    Update inference_realesrgan_video.py

--- inference_realesrgan_video.py
+++ inference_realesrgan_video.py
@@ -238,11 +238,17 @@ def inference_video(args, video_save_path, device=None, total_workers=1, worker_
     if args.face_enhance:  # Use GFPGAN for face enhancement
         from gfpgan import GFPGANer
         face_enhancer = GFPGANer(
-            model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth',
+            model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth',
             upscale=args.outscale,
-            arch='clean',
+            arch='RestoreFormer',
             channel_multiplier=2,
-            bg_upsampler=upsampler)  # TODO support custom device
+            bg_upsampler=upsampler)
+        # face_enhancer = GFPGANer(
+            # model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth',
+            # upscale=args.outscale,
+            # arch='clean',
+            # channel_multiplier=2,
+            # bg_upsampler=upsampler)  # TODO support custom device
     else:
         face_enhancer = None
 
