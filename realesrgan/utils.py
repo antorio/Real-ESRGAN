57c4671af947bed0bb3192303e10a5fe615a184b
antorio <antorio.bergasdito@gmail.com>
2025-01-17 11:26:41 +0700

    Update utils.py

--- realesrgan/utils.py
+++ realesrgan/utils.py
@@ -74,6 +74,11 @@ class RealESRGANer():
         if self.half:
             self.model = self.model.half()
 
+        if torch.__version__[0] >= '2':
+            self.model = self.model.to(memory_format=torch.channels_last)
+            print(torch.__version__[0])
+            self.model = torch.compile(self.model)
+
     def dni(self, net_a, net_b, dni_weight, key='params', loc='cpu'):
         """Deep network interpolation.
 
