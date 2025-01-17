4f90dee290607ac3b3be7bb01f660a0436e4a509
antorio <antorio.bergasdito@gmail.com>
2025-01-17 13:02:06 +0700

    Update utils.py

--- realesrgan/utils.py
+++ realesrgan/utils.py
@@ -85,8 +85,8 @@ class RealESRGANer():
         ``Paper: Deep Network Interpolation for Continuous Imagery Effect Transition``
         """
         # loc = self.device
-        net_a = torch.load(net_a, map_location=torch.device(self.device))
-        net_b = torch.load(net_b, map_location=torch.device(self.device))
+        net_a = torch.load(net_a, map_location=torch.device(self.device), weights_only=True)
+        net_b = torch.load(net_b, map_location=torch.device(self.device), weights_only=True)
         for k, v_a in net_a[key].items():
             net_a[key][k] = dni_weight[0] * v_a + dni_weight[1] * net_b[key][k]
         return net_a
