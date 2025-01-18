a229fc58ca766eb1945d75285f5ee291bab3b365
antorio <antorio.bergasdito@gmail.com>
2025-01-18 13:35:09 +0700

    Update inference_realesrgan_video.py

--- inference_realesrgan_video.py
+++ inference_realesrgan_video.py
@@ -196,9 +196,13 @@ def inference_video(args, video_save_path, device=None, total_workers=1, worker_
         netscale = 4
         file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
     elif args.model_name == '4x_NMKD-Superscale-SP_178000_G': 
-        # model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
         netscale = 4
         file_url = ['https://huggingface.co/SDExplains/Upscaler/resolve/3b6ebcf0b52425d2e36be6a6d8ccda44d47bb0e7/4x_NMKD-Superscale-SP_178000_G.pth']
+    elif args.model_name == '4x-UltraSharp': 
+        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+        netscale = 4
+        file_url = ['https://huggingface.co/lokCX/4x-Ultrasharp/resolve/main/4x-UltraSharp.pth']
     elif args.model_name == 'RealESRNet_x4plus':  # x4 RRDBNet model
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
         netscale = 4
